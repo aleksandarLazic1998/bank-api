6e9bb24a11e3093001dfab51dd86cdfa942170d7
aleksandarLazic1998 <aleksandar.lazic.p@gmail.com>
2022-03-06 19:12:15 +0100

    Add Banker Clients Connection

--- src/Router/Banker/router.ts
+++ src/Router/Banker/router.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import ROUTES from "../../Routes";
-import { createBanker, editBanker } from "./controllers";
+import { createBanker, editBanker, connectClientBanker } from "./controllers";
 
 const BankerRouter = express.Router();
 
 BankerRouter.post(ROUTES.BANKER.BASE, createBanker);
 BankerRouter.put(`${ROUTES.BANKER.BASE}/:bankerId`, editBanker);
+BankerRouter.put(
+	`${ROUTES.BANKER.BASE}/:bankerId/client/:clientId`,
+	connectClientBanker
+);
 
 export default BankerRouter;
