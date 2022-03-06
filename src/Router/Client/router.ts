b42a35919f86ba012c91cad1601bee21248de471
aleksandarLazic1998 <aleksandar.lazic.p@gmail.com>
2022-03-06 19:40:57 +0100

    Add fetching client by id

--- src/Router/Client/router.ts
+++ src/Router/Client/router.ts
@@ -1,10 +1,12 @@
 import express, { Request, RequestHandler } from "express";
 import ROUTES from "../../Routes";
-import { createUser, editUser } from "./controllers";
+import { createUser, editUser, getAllClients, getClient } from "./controllers";
 
 const ClientRouter = express.Router();
 
 ClientRouter.post(ROUTES.CLIENT.BASE, createUser);
 ClientRouter.put(`${ROUTES.CLIENT.BASE}/:clientId`, editUser);
+ClientRouter.get(ROUTES.CLIENT.BASE, getAllClients);
+ClientRouter.get(`${ROUTES.CLIENT.BASE}/:clientId`, getClient);
 
 export default ClientRouter;
